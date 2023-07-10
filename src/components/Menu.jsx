45303cf538ae56cd11116af8648c768c4553dd4e
Dmitry Levchenko <13d@ro.ru>
2023-07-11 02:23:30 +0700

    Make menu hidable

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function Menu({
     isNextPlayerMarked,
     onMarkNextPlayer,
@@ -11,77 +13,111 @@ function Menu({
     testData,
     onChangeTestCase,
 }) {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    function handleOpenMenu() {
+        setIsMenuOpen((currentState) => !currentState);
+    }
+
     return (
-        <div className="menu">
-            <button className="menu-button" onClick={onMarkNextPlayer}>
-                {isNextPlayerMarked
-                    ? "Not mark next player"
-                    : "Mark next player"}
-            </button>
-
-            <select
-                className="menu-select"
-                value={boardSide}
-                onChange={(event) => onSelectBoardSide(event.target.value)}
-            >
-                <option value="auto">Board side: auto</option>
-                <option value="white">Board side: white</option>
-                <option value="black">Board side: black</option>
-                <option value="90">Board side: 90°</option>
-            </select>
-
-            {history.length > 1 && (
-                <button className="menu-button" onClick={onUndoLastMove}>
-                    &#x21D0; Undo last move
-                </button>
-            )}
-
-            {history.length > 2 && (
-                <select
-                    className="menu-select"
-                    onChange={(event) => onGoBackInHistory(+event.target.value)}
+        <>
+            <div className="menu">
+                <a
+                    href="#"
+                    className={`menu-burger ${isMenuOpen && "open"}`}
+                    onClick={handleOpenMenu}
                 >
-                    <option value="">History:</option>
-                    {history
-                        .filter((_, index) => index !== 0)
-                        .map((_, index) => (
-                            <option
-                                value={index}
-                                key={index}
-                                style={
-                                    index === 0
-                                        ? { color: "red" }
-                                        : { color: "rgb(255, 113, 70)" }
+                    <span></span>
+                </a>
+
+                {isMenuOpen && (
+                    <div className="menu-items">
+                        <select
+                            className="menu-select"
+                            value={boardSide}
+                            onChange={(event) =>
+                                onSelectBoardSide(event.target.value)
+                            }
+                        >
+                            <option value="auto">Board side: auto</option>
+                            <option value="white">Board side: white</option>
+                            <option value="black">Board side: black</option>
+                            <option value="90">Board side: 90°</option>
+                        </select>
+
+                        <button
+                            className="menu-button"
+                            onClick={onMarkNextPlayer}
+                        >
+                            {isNextPlayerMarked
+                                ? "🚩Not mark next player"
+                                : "🚩Mark next player"}
+                        </button>
+
+                        {history.length > 1 && (
+                            <button
+                                className="menu-button"
+                                onClick={onUndoLastMove}
+                            >
+                                &#x21D0; Undo last move
+                            </button>
+                        )}
+
+                        {history.length > 2 && (
+                            <select
+                                className="menu-select"
+                                onChange={(event) =>
+                                    onGoBackInHistory(+event.target.value)
                                 }
                             >
-                                {index === 0
-                                    ? "-- Start game --"
-                                    : `- go to move ${index + 1}`}
-                            </option>
-                        ))}
-                </select>
-            )}
+                                <option value="">History:</option>
+                                {history
+                                    .filter((_, index) => index !== 0)
+                                    .map((_, index) => (
+                                        <option
+                                            value={index}
+                                            key={index}
+                                            style={
+                                                index === 0
+                                                    ? { color: "red" }
+                                                    : {
+                                                          color: "rgb(255, 113, 70)",
+                                                      }
+                                            }
+                                        >
+                                            {index === 0
+                                                ? "-- Start game --"
+                                                : `- go to move ${index + 1}`}
+                                        </option>
+                                    ))}
+                            </select>
+                        )}
 
-            <button className="menu-button" onClick={onSetDevMode}>
-                {isDevMode ? "👈 Quit dev move" : "🛠 Dev mode"}
-            </button>
+                        <button className="menu-button" onClick={onSetDevMode}>
+                            {isDevMode ? "👈 Quit dev move" : "🛠 Dev mode"}
+                        </button>
 
-            {isDevMode && (
-                <select
-                    className="menu-select"
-                    onChange={(event) => onChangeTestCase(event.target.value)}
-                >
-                    <option value="">Test Case:</option>
-                    {testData.map((data, index) => (
-                        <option
-                            style={{ color: "rgb(255, 113, 70)" }}
-                            value={index}
-                            key={index}
-                        >{`- case ${index + 1}`}</option>
-                    ))}
-                </select>
-            )}
-        </div>
+                        {isDevMode && (
+                            <select
+                                className="menu-select"
+                                onChange={(event) =>
+                                    onChangeTestCase(event.target.value)
+                                }
+                            >
+                                <option value="">🔥 Test Case:</option>
+                                {testData.map((data, index) => (
+                                    <option
+                                        style={{ color: "rgb(255, 113, 70)" }}
+                                        value={index}
+                                        key={index}
+                                    >{`- case ${index + 1}`}</option>
+                                ))}
+                            </select>
+                        )}
+                    </div>
+                )}
+            </div>
+        </>
     );
 }
 
